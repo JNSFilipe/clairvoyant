2db73e4a79590373385a1c407f6978821139031f
JNSFilipe <jose.filipe@ieee.org>
2025-02-05 22:33:58 +0000

    Fixed indentation with black and adhered to python conventions

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from distutils.core import setup
 
-setup(name='clairvoyant',
-      version='v1.0',
-      description='Software designed to identify and monitor social/historical cues for short term stock movement',
-      author='Anthony Federico',
-      author_email='dephoona@gmail.com',
-      url='https://github.com/anfederico/Clairvoyant',
-      packages=['clairvoyant']
-     )
+setup(
+    name="clairvoyant",
+    version="v1.0",
+    description="Software designed to identify and monitor social/historical cues for short term stock movement",
+    author="JNSFilipe",
+    author_email="jose.filipe@ieee.org",
+    url="https://github.com/JNSFilipe/Clairvoyant",
+    packages=["clairvoyant"],
+)
--- tests/testexchange.py
+++ tests/testexchange.py
@@ -2,20 +2,22 @@ import unittest
 
 # Local imports
 import sys
+
 sys.path.append("../.")
 from clairvoyant import exchange, helpers
+
 
 class Methods(unittest.TestCase):
 
     def test_errors(self):
         a = exchange.Account(1000)
-        self.assertRaises(ValueError, a.EnterPosition, 'Long',  2000, 10)
-        self.assertRaises(ValueError, a.EnterPosition, 'Long',  -500, 10)       
-        self.assertRaises(ValueError, a.EnterPosition, 'Long', 500, -10)
+        self.assertRaises(ValueError, a.EnterPosition, "Long", 2000, 10)
+        self.assertRaises(ValueError, a.EnterPosition, "Long", -500, 10)
+        self.assertRaises(ValueError, a.EnterPosition, "Long", 500, -10)
         # Enter valid position
-        a.EnterPosition('Long', 250, 10)
-        a.EnterPosition('Short', 250, 10)
-        Long  = a.Positions[0]
+        a.EnterPosition("Long", 250, 10)
+        a.EnterPosition("Short", 250, 10)
+        Long = a.Positions[0]
         Short = a.Positions[1]
         self.assertRaises(ValueError, a.ClosePosition, Long, 0.5, -20)
         self.assertRaises(ValueError, a.ClosePosition, Long, 1.01, 20)
@@ -26,8 +28,8 @@ class Methods(unittest.TestCase):
     def test_long(self):
         a = exchange.Account(1000)
         # Win on a long
-        a.EnterPosition('Long', 500, 10)
-        a.EnterPosition('Long', 500, 10)
+        a.EnterPosition("Long", 500, 10)
+        a.EnterPosition("Long", 500, 10)
         self.assertEqual(a.BuyingPower, 0)
         self.assertEqual(a.TotalValue(10), 1000)
         L0 = a.Positions[0]
@@ -41,7 +43,7 @@ class Methods(unittest.TestCase):
         self.assertEqual(a.BuyingPower, 2000)
         self.assertEqual(a.TotalValue(40), 3000)
         # Lose on a long
-        a.EnterPosition('Long', 1000, 50)
+        a.EnterPosition("Long", 1000, 50)
         L2 = a.Positions[2]
         a.ClosePosition(L2, 0.5, 25)
         self.assertEqual(a.BuyingPower, 1250)
@@ -49,9 +51,9 @@ class Methods(unittest.TestCase):
 
     def test_short(self):
         a = exchange.Account(1000)
-        # Win on a short        
-        a.EnterPosition('Short', 500, 10)
-        a.EnterPosition('Short', 500, 10)
+        # Win on a short
+        a.EnterPosition("Short", 500, 10)
+        a.EnterPosition("Short", 500, 10)
         self.assertEqual(a.BuyingPower, 0)
         self.assertEqual(a.TotalValue(10), 1000)
         S0 = a.Positions[0]
@@ -64,8 +66,8 @@ class Methods(unittest.TestCase):
         a.ClosePosition(S1, 0.5, 2.5)
         self.assertEqual(a.BuyingPower, 1187.5)
         self.assertEqual(a.TotalValue(2.5), 1625)
-        # Lose on a short   
-        a.EnterPosition('Short', 1000, 2)
+        # Lose on a short
+        a.EnterPosition("Short", 1000, 2)
         S2 = a.Positions[2]
         a.ClosePosition(S2, 0.5, 4)
         self.assertEqual(a.BuyingPower, 187.5)
@@ -73,17 +75,17 @@ class Methods(unittest.TestCase):
 
     def test_both(self):
         a = exchange.Account(1000)
-        a.EnterPosition('Long',  200, 20)
-        a.EnterPosition('Short', 250, 25)
+        a.EnterPosition("Long", 200, 20)
+        a.EnterPosition("Short", 250, 25)
         self.assertEqual(a.BuyingPower, 550)
         self.assertEqual(a.TotalValue(25), 1050)
-        Long  = a.Positions[0]
+        Long = a.Positions[0]
         Short = a.Positions[1]
-        a.ClosePosition(Long,  0.5, 40)
+        a.ClosePosition(Long, 0.5, 40)
         a.ClosePosition(Short, 0.5, 12.5)
         self.assertEqual(a.BuyingPower, 937.5)
         self.assertEqual(a.TotalValue(12.5), 1187.5)
-        a.ClosePosition(Long,  1.0, 50)
+        a.ClosePosition(Long, 1.0, 50)
         a.ClosePosition(Short, 1.0, 50)
         self.assertEqual(a.BuyingPower, 1187.5)
         self.assertEqual(a.TotalValue(100), 1187.5)
@@ -91,16 +93,17 @@ class Methods(unittest.TestCase):
     def test_decimals(self):
         # Long with decimals
         a = exchange.Account(2)
-        a.EnterPosition('Long', 1, 0.00000001)
+        a.EnterPosition("Long", 1, 0.00000001)
         self.assertEqual(a.TotalValue(0.00000002), 3)
         a.ClosePosition(a.Positions[0], 1, 0.00000002)
         self.assertEqual(a.BuyingPower, 3)
-        # Short with decimals 
+        # Short with decimals
         a = exchange.Account(2)
-        a.EnterPosition('Short', 1, 0.00000002)
+        a.EnterPosition("Short", 1, 0.00000002)
         self.assertEqual(a.TotalValue(0.00000001), 2.5)
         a.ClosePosition(a.Positions[0], 1, 0.00000001)
         self.assertEqual(a.BuyingPower, 2.5)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
